b316b8b52ac052a9571d2a3ab30c61530148316a
Michael Nutt <michael@nuttnet.net>
2008-12-22 12:49:14 -0600

    Redirect output to log/development.log

--- scripting/merb/config/environments/development.rb
+++ scripting/merb/config/environments/development.rb
@@ -7,9 +7,9 @@ Merb::Config.use { |c|
   c[:ignore_tampered_cookies] = true
   c[:log_auto_flush ] = true
   c[:log_level] = :debug
-
-  c[:log_stream] = STDOUT
-  c[:log_file]   = nil
-  # Or redirect logging into a file:
-  # c[:log_file]  = Merb.root / "log" / "development.log"
+  c[:verbose] = :true
+  # c[:log_stream] = STDOUT
+  # c[:log_file]   = nil
+  # redirect logging into a file:
+  c[:log_file]  = Merb.root / "log" / "development.log"
 }
