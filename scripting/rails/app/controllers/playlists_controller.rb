bebf967ddea5f3ff026816c6505b69591f99b033
Michael Nutt <michael@nuttnet.net>
2009-01-05 19:18:31 -0500

    Add some REST methods to playlists controller

--- scripting/rails/app/controllers/playlists_controller.rb
+++ scripting/rails/app/controllers/playlists_controller.rb
@@ -1,6 +1,32 @@
 class PlaylistsController < ApplicationController
+  self.allow_forgery_protection = false
+
   def index
     @playlists = Playlist.find(:all)
-    render :json => @playlists.to_json
+    @ordered_playlists = []
+    @playlists.each_with_index do |p, i| 
+      @ordered_playlists << { :playlist => p.to_playlist, :position => i, :is_owner => true } 
+    end
+
+    render :json => @ordered_playlists.to_json()
+  end
+
+  def create
+    @playlist = Playlist.new(:name => params[:name])
+    @playlist.save
+    render :text => { :playlist => @playlist.to_playlist, :position => Playlist.count }.to_json
+  end
+
+  def destroy
+    @playlist = Playlist.find(params[:id])
+    @playlist.destroy
+    render :json => {:response => 200}
+  end
+
+  def update
+    @playlist = Playlist.find(params[:id])
+    @playlist.name = params[:name]
+    @playlist.save
+    render :json => {:response => 200}
   end
 end
