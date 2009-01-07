0c34b348aba472388e8234211d0d48f0e85033ac
Michael Nutt <michael@nuttnet.net>
2009-01-07 17:26:32 -0500

    Add ability to pass in 'ids' array to find specific ids (for regular playlists)

--- scripting/rails/lib/limewire.rb
+++ scripting/rails/lib/limewire.rb
@@ -157,6 +157,10 @@ module Limewire
     rescue
       nil
     end
+
+    def self.find_by_sha1s(sha1s)
+      sha1s.collect{ |sha1| self.find_by_sha1(sha1) }.compact
+    end
     
     def self.categories
       Limewire.core.get_file_manager.get_managed_file_list.managed_categories rescue []
