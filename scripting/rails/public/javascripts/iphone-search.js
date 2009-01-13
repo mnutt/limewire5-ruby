7c1ee0b6946fcb90fd5a2b99c2af17aee8df15e9
Michael Nutt <michael@nuttnet.net>
2009-01-13 00:40:55 -0500

    Add progress bar to iphone search interface

--- scripting/rails/public/javascripts/iphone-search.js
+++ scripting/rails/public/javascripts/iphone-search.js
@@ -1,8 +1,10 @@
+var blah;
 jQuery(document).ready(function(){
   var submitSearch = function() {
 
   var startDownload = function(ev) {
-    li = $("#"+ev.target.id);
+    blah = ev.target;
+    li = $(ev.target).parent();
     li.addClass("downloading");
     magnet = li.find('input[name=magnet]').val();
     $.post("/downloads", { magnet: magnet });
@@ -10,13 +12,15 @@ jQuery(document).ready(function(){
     $.periodic(function(controller) {
       if(done) {
 	li.click(function() {
-	  window.location = "/library/"+ev.target.id+".mp3";
+	  window.location = "/library/"+li.attr('id')+".mp3";
 	  return false;
 	});
 	controller.stop();
       }
-      $.getJSON("/downloads/"+ev.target.id, function(download) {
+      $.getJSON("/downloads/"+li.attr('id'), function(download) {
 	li.find('.percent').text(download.percent_complete);
+		  console.log(download.percent_complete);
+	li.find('.progress').css({ width: download.percent_complete+"%" });
 	if(download.percent_complete == 100) { done = true; }
       });
       return true;
@@ -41,7 +45,13 @@ jQuery(document).ready(function(){
 
 	  $.each(results, function(index, result) {
 	    if($('#'+result.sha1).length == 0) {
-	      $('#results').append("<li class='result' id="+result.sha1+">"+result.filename+"<input type='hidden' name='magnet' value='"+result.magnet_url+"'/><div class='percent'></div></li>");
+	      item =  "<li class='result' id="+result.sha1+">";
+	      item += "  <div class='filename'>"+result.filename+"</div>";
+	      item += "  <input type='hidden' name='magnet' value='"+result.magnet_url+"'/>";
+	      item += "  <div class='percent'></div>";
+	      item += "  <div class='progress'></div>";
+	      item += "</li>";
+	      $('#results').append(item);
 	      $('#results li:last').click(startDownload);
 	    }
 	    if($('#results li').length > 50) {
