8247ebbf6c4e6305787ddeb006f31e9f36ae7eca
Michael Nutt <michael@nuttnet.net>
2009-01-16 19:55:13 -0500

    Add some stuff to backup controller

--- scripting/rails/app/controllers/backup_controller.rb
+++ scripting/rails/app/controllers/backup_controller.rb
@@ -5,7 +5,11 @@ class BackupController < ApplicationController
   def create
     @s3 = RightAws::S3.new(params[:aws_key], params[:aws_secret])
     @bucket = @s3.bucket("limewire-backup", true)
-    raise @bucket.to_s
+    
+    # @files = Limewire::Library.find(:all)
+    # @files.each do |f|
+    #   @bucket.put("#{f.sha1}--#{f.get_file.absolute_path}", File.open(f.get_file.absolute_path))
+    # end
   end
 
   def show
