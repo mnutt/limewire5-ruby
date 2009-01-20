7478e207d058280294f2455ddb5d0bb102070e14
Michael Nutt <michael@nuttnet.net>
2009-01-20 02:33:51 -0500

    Remove merb (we can always resurrect it later if we decide to switch back)

--- scripting/merb/app/helpers/cloud_helper.rb
+++
@@ -1,5 +0,0 @@
-module Merb
-  module CloudHelper
-
-  end
-end # Merb
--- scripting/merb/spec/requests/cloud_spec.rb
+++
@@ -1,7 +0,0 @@
-require File.join(File.dirname(__FILE__), '..', 'spec_helper.rb')
-
-describe "/cloud" do
-  before(:each) do
-    @response = request("/cloud")
-  end
-end
