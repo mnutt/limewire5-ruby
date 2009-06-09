a491af637929b2245ccd52917f59da58b25af26a
Michael Nutt <michael@nuttnet.net>
2009-06-09 17:39:18 -0400

    Add more options to the Remote Options Panel

--- private-components/swingui/src/main/java/org/limewire/ui/swing/options/RemoteOptionPanel.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/options/RemoteOptionPanel.java
@@ -7,12 +7,15 @@ import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 import net.miginfocom.swing.MigLayout;
 
 import org.jdesktop.application.Resource;
 import org.limewire.http.mongrel.MongrelManager;
 import org.limewire.http.mongrel.MongrelGlue;
+import org.limewire.http.mongrel.WebSettings;
 import org.limewire.ui.swing.settings.SwingUiSettings;
 import org.limewire.ui.swing.util.GuiUtils;
 import org.limewire.ui.swing.util.I18n;
@@ -27,6 +30,7 @@ import com.google.inject.Inject;
 public class RemoteOptionPanel extends OptionPanel {
 
     private ServicePanel servicePanel;
+    private WebPanel webPanel;
     
     private MongrelManager mongrelManager;
     
@@ -42,6 +46,7 @@ public class RemoteOptionPanel extends OptionPanel {
         setLayout(new MigLayout("insets 15 15 15 15, fillx, wrap", "", ""));
         
         add(getServicePanel(), "pushx, growx");
+        add(getWebPanel(), "pushx, growx");
     }
 
     private OptionPanel getServicePanel() {
@@ -50,20 +55,28 @@ public class RemoteOptionPanel extends OptionPanel {
         }
         return servicePanel;
     }
+    
+    private OptionPanel getWebPanel() {
+        if(webPanel == null) {
+            webPanel = new WebPanel();
+        }
+        return webPanel;
+    }
 
     @Override
     boolean applyOptions() {
-        return getServicePanel().applyOptions();
+        return getServicePanel().applyOptions() || getWebPanel().applyOptions();
     }
 
     @Override
     boolean hasChanged() {        
-        return getServicePanel().hasChanged();
+        return getServicePanel().hasChanged() || getWebPanel().hasChanged();
     }
 
     @Override
     public void initOptions() {
         getServicePanel().initOptions();
+        getWebPanel().initOptions();
     }
 
     private class ServicePanel extends OptionPanel {
@@ -85,9 +98,9 @@ public class RemoteOptionPanel extends OptionPanel {
             stopButton.addActionListener(new StopRemoteAction(mongrelManager));
 
             add(statusLabel, "gapbottom 5");
-            // add(status, "gapbottom 5, wrap");
-            // add(startButton, "gapbottom 5");
-            // add(stopButton, "gapbottom 5, wrap");
+            add(status, "gapbottom 5, wrap");
+            add(startButton, "gapbottom 5");
+            add(stopButton, "gapbottom 5, wrap");
             add(startOnLaunchCheckBox, "wrap");
         }
 
@@ -105,6 +118,44 @@ public class RemoteOptionPanel extends OptionPanel {
         @Override
         public void initOptions() {
             startOnLaunchCheckBox.setSelected(SwingUiSettings.START_REMOTE_ON_LAUNCH.getValue());
+        }
+    }
+    private class WebPanel extends OptionPanel {
+
+        private JPasswordField passwordField;
+        private JTextField usernameField;
+        
+        public WebPanel() {
+            super(tr("WebUI"));
+
+            passwordField = new JPasswordField(30);
+            usernameField = new JTextField(30);
+            JLabel passwordLabel = new JLabel(I18n.tr("Password:"));
+            JLabel usernameLabel = new JLabel(I18n.tr("Username:"));
+
+            add(passwordLabel, "gapbottom 5");
+            add(passwordField, "gapbottom 5, wrap");
+            add(usernameLabel);
+            add(usernameField, "wrap");
+        }
+
+        @Override
+        boolean applyOptions() {
+            WebSettings.WEB_PASSWORD.set(passwordField.getPassword().toString());
+            WebSettings.WEB_ADDRESS.set(usernameField.getText());
+            return false;
+        }
+
+        @Override
+        boolean hasChanged() {
+            return passwordField.getPassword().toString() != WebSettings.WEB_PASSWORD.getValueAsString() ||
+                   usernameField.getText() != WebSettings.WEB_ADDRESS.getValueAsString();
+        }
+
+        @Override
+        public void initOptions() {
+            passwordField.setText(WebSettings.WEB_PASSWORD.getValueAsString()); 
+            usernameField.setText(WebSettings.WEB_ADDRESS.getValueAsString());
         }
     }
 }
